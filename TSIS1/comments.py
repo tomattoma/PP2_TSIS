8074e7efceb433043e8567566c07a48d351a0d04
tomattoma <t_alpysbay@kbtu.kz>
2025-01-14 03:02:49 +0500

    dop

--- TSIS1/comments.py
+++ TSIS1/comments.py
@@ -1,7 +1,12 @@
 
-itoshi=18
+itoshi=18 
 bachira=17
 
 #oh itoshi older than bachira :0
 
 print((itoshi+bachira)*2) #thats 70 
+
+if itoshi==10:
+    print("Its impossible")
+else:
+    print("hello world")
