b7ca89b39537cfdb6207e25077927f606d315ee1
tomattoma <t_alpysbay@kbtu.kz>
2025-01-21 10:51:27 +0500

    upd

--- TSIS1/python_strings.py
+++ TSIS1/python_strings.py
@@ -6,3 +6,28 @@ consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(tom)
+
+b = "Hello, World!"
+print(b[5:9])
+
+madina = "Hello toma"
+print(madina.upper())
+
+aruzhan = "Hello TOMA"
+print(aruzhan.lower())
+
+ayana = "    HELLO HELLO   "
+print(ayana.lstrip())
+
+mountain = "Madina I love you"
+print(mountain.replace('M','m'))
+
+airport = "I love my lady"
+print(airport.split(' '))
+
+temp = 33
+roman=f"Actually I am {temp}"
+print(roman)
+
+jungkook = "annyeong-haseyo we are \"bts\"!"
+print(jungkook)
