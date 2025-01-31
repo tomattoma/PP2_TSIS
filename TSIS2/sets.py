249d76d29133011a8da3170a9fcba5195d214043
tomattoma <t_alpysbay@kbtu.kz>
2025-02-01 02:46:34 +0500

    'upd'

---
+++ TSIS2/sets.py
@@ -0,0 +1,13 @@
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+
+myset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+myset.update(tropical)
+
+print(myset)
--- TSIS2/tuples.py
+++ TSIS2/tuples.py
@@ -2,3 +2,12 @@ thistuple = ("apple", "banana", "cherry")
 y = list(thistuple)
 y.append("orange")
 thistuple = tuple(y)
+
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
